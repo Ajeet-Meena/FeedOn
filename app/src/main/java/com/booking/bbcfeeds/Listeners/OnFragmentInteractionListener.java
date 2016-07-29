e8edfb4022dcc2191d067d624fad15bf1111a50d
Ajk2rxKamehameha <ajeet.meena@gmail.com>
2016-07-29 15:43:31 +0530

    Added AppBarObserver and OnFragmentInteractionListener.

---
+++ app/src/main/java/com/booking/bbcfeeds/Listeners/OnFragmentInteractionListener.java
@@ -0,0 +1,8 @@
+package com.booking.bbcfeeds.Listeners;
+
+/**
+ * Created by Ajeet Kumar Meena on 18-06-2016.
+ */
+public interface OnFragmentInteractionListener {
+    void showDrawerToggle(boolean showDrawerToggle);
+}
