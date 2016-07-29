d9d59ec4c4bfa1cd712b062955d489bdac907bb5
Ajk2rxKamehameha <ajeet.meena@gmail.com>
2016-07-29 16:11:22 +0530

    Added Application Context

--- app/src/main/java/com/booking/bbcfeeds/MyApplication.java
+++ app/src/main/java/com/booking/bbcfeeds/MyApplication.java
@@ -2,19 +2,11 @@ package com.booking.bbcfeeds;
 
 import android.app.Application;
 
+import com.booking.bbcfeeds.Utils.FontsOverride;
 import com.squareup.picasso.LruCache;
 import com.squareup.picasso.OkHttpDownloader;
 import com.squareup.picasso.Picasso;
-import com.ajeet_meena.movieon.Api.APIService;
-import com.ajeet_meena.movieon.Api.RestClient;
-import com.ajeet_meena.movieon.Utils.FontsOverride;
 
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
-import okhttp3.Interceptor;
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 
 
 /**
@@ -22,52 +14,14 @@ import okhttp3.logging.HttpLoggingInterceptor;
  */
 public class MyApplication extends Application {
 
-    private static final int READ_TIMEOUT = 60 * 1000;
-    private static final int CONNECTION_TIMEOUT = 60 * 1000;
     private static MyApplication myApplication;
-    private static RestClient restClient;
-    private static OkHttpClient okHttpClient;
     public final String TAG = MyApplication.class.getSimpleName();
-
-    private static RestClient getRestClient() {
-        if (restClient == null) {
-            restClient = new RestClient();
-        }
-        return restClient;
-    }
 
     public static synchronized MyApplication getInstance() {
         return myApplication;
     }
 
-    public static OkHttpClient getOkHttpClient() {
-        if (okHttpClient == null) {
-            HttpLoggingInterceptor logger = new HttpLoggingInterceptor();
-            logger.setLevel(BuildConfig.DEBUG ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.NONE);
-            okHttpClient = new OkHttpClient.Builder().cache(getCache(4)).retryOnConnectionFailure(true).readTimeout(READ_TIMEOUT, TimeUnit.MILLISECONDS)
-                    .connectTimeout(CONNECTION_TIMEOUT, TimeUnit.MILLISECONDS).addInterceptor(logger).addInterceptor(new Interceptor() {
-                        @Override
-                        public okhttp3.Response intercept(Chain chain) throws IOException {
-                            okhttp3.Request request = chain.request().newBuilder()
-                                    .addHeader("Accept", "application/json")
-                                    .addHeader("Content-type", "application/json").build();
-                            return chain.proceed(request);
-                        }
-                    }).build();
 
-        }
-        return okHttpClient;
-    }
-
-    private static okhttp3.Cache getCache(int size_in_mb) {
-        int cacheSize = size_in_mb * 1024 * 1024; //10MB
-        okhttp3.Cache cache = new okhttp3.Cache(getInstance().getCacheDir(), cacheSize);
-        return cache;
-    }
-
-    public static APIService getAPIService() {
-        return getRestClient().getApiService();
-    }
 
     @Override
     public void onCreate() {
