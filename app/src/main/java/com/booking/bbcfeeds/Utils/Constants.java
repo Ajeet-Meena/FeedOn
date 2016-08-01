9853d81615b11cafa6ea93bac7f95e0e149da005
Ajk2rxKamehameha <ajeet.meena@gmail.com>
2016-08-01 20:55:14 +0530

    Added default feed loader to download pre loaded feeds. Added preloaded feeds in constants

---
+++ app/src/main/java/com/booking/bbcfeeds/Utils/Constants.java
@@ -0,0 +1,14 @@
+package com.booking.bbcfeeds.Utils;
+
+/**
+ * Created by Ajeet Kumar Meena on 01-08-2016.
+ */
+
+public class Constants {
+    public static final String[] WEB_SITES_LIST = {"https://feeds.feedburner.com/TechCrunch/",
+            "http://www.cnet.com/rss/news/",
+            "http://feeds.feedburner.com/codinghorror?format=xml",
+            "http://9gag-rss.com/api/rss/get?code=9GAG&format=2",
+            "http://www.smbc-comics.com/rss.php",
+    };
+}
--- app/src/main/java/com/booking/bbcfeeds/Utils/Contstants.java
+++
@@ -1,9 +0,0 @@
-package com.booking.bbcfeeds.Utils;
-
-/**
- * Created by Ajeet Kumar Meena on 01-08-2016.
- */
-
-public class Contstants {
-    public static final String[] WEB_SITES_LIST = {};
-}
